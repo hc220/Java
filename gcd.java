9db1c1dec32d6eeb63090fc452a6c0782df25a7a
Harshil Patel <harshil220205@gmail.com>
2025-01-29 23:46:11 +0530

    Add files via upload

---
+++ gcd.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class gcd{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int result = gcd(a, b);
+        System.out.println(result);
+
+    }
+    public static int gcd(int a,int b){
+        if(b==0){
+            return a;
+            }
+            else{
+                return gcd(b,a%b);
+            }
+    }
+}
